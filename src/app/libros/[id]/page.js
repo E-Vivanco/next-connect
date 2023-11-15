b97c2164c302a3d2d36c95e5870c7869c132ca81
Elsa Beltran <vivanco.elsa@gmail.com>
2023-11-15 06:37:41 -0300

    Recepcion_params_libro

--- src/app/libros/[id]/page.js
+++ src/app/libros/[id]/page.js
@@ -2,7 +2,10 @@ export default function LibroIDpage({params}) {
     console.log(params)
   return (
     <main>
-       <h1>Hello </h1>
+       <h1>
+        Hello 
+        Lectura de parametro `${params}`
+       </h1>
     </main>
   )
 }
