d64c215a54ee3e4cd7bd3283ad1968176a592c88
Elsa Beltran <vivanco.elsa@gmail.com>
2023-11-15 06:01:52 -0300

    subir_a_vercel

--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,7 +1,7 @@
-export default function getDomain(){
+/*export default function getDomain(){
     const protocol = process.env.DEVELOPMENT_ENV ===
     "production" ? "https" : "http"
     const domain = process.env.PUBLIC_DOMAIN ?  process.env.
     PUBLIC_DOMAIN: "localhost:3000"
     return `${protocol}://${domain}`
-}
+}*/
