125d02696b6f92df90fbf64572a5e9019fe07025
Elsa Beltran <vivanco.elsa@gmail.com>
2023-11-16 09:02:26 -0300

    act_tabla_1

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -11,34 +11,16 @@ export async function helloWorld(){
     return  {dbNow:"hello",dbNow: Math.abs(end - start )} //dbResponse
 }
 
-export async function configureDatabase(){
-    const dbResponse =  sql`
-        CREATE TABLE IF NOT EXISTS "libro"(
-            "id" SERIAL NOT NULL,
-           "_id" integer DEFAULT 0,
-            "title" character varying(255) DEFAULT 'null'::text,
-            "isbn" character varying(255) DEFAULT 'null'::text,
-            "pagecount" integer DEFAULT 0,
-            "publisheddate" date DEFAULT '1900-04-01'::date,
-            "thumbnailurl" text DEFAULT 'null'::text,
-            "shortdescription" text DEFAULT 'null'::text,
-            "longdescription" text DEFAULT 'null'::text,
-            "status" character varying(255) DEFAULT 'null'::text,
-            "calificacion" integer DEFAULT 0,
-            PRIMARY KEY(id)       
-    )`
-
-    const dbResponse2 = sql`
-        CREATE TABLE IF NOT EXISTS "author"(
+async function configureDatabase(){
+    const dbResponse = await sql`
+            CREATE TABLE IF NOT EXISTS "author"(
             "id" SERIAL NOT NULL,
             "nombre_author" text DEFAULT 'null'::text,
-            "id_libro" integer,
+            "id_libro" integer UNIQUE,
             PRIMARY KEY(id)
-    )
-    CREATE UNIQUE INDEX author_id_libro_key ON "author" USING btree ("id_libro")
     )`
 
-    console.log("Db response for new table-1",dbResponse,dbResponse2)
+    console.log("Db response for new table-1",dbResponse)
    // console.log("Db response for new table-2",dbResponse2)
 }
 configureDatabase().catch(err=>console.log("db config err",err))
